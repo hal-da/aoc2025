29be104facb7402ae6da3dd40d2fdb3e7070c075
Daniel Halasz <daniel.halasz@sitcons.com>
2025-12-05 14:02:51 +0100

    day05 - puzzle2

--- src/day05/main.js
+++ src/day05/main.js
@@ -1,6 +1,6 @@
-import { test, input } from './input.js';
+import {test, data} from './input.js';
 
-let [ranges, ids] = input
+let [ranges, ids] = data
     .split('\n\n')
     .map(
         (line) => line
@@ -9,53 +9,54 @@ let [ranges, ids] = input
 ids = ids.map(Number);
 ranges = ranges.map(range => range.split('-').map(Number));
 
-const puzzle1 = () =>{
+const puzzle1 = () => {
     let count = {}
     ids.forEach(id => {
         ranges.forEach(range => {
-            const [lower,upper] = range
-            if (id >= lower && id <= upper){
-                if(count[id])count[id]++;
-                else count[id] = 1;
+            const [lower, upper] = range
+            if (id >= lower && id <= upper) {
+                count[id] = true;
             }
         })
     })
-    // console.log(Object.keys(count).length);
+    console.log(Object.keys(count).length);
 }
 
 puzzle1();
 
+const puzzle2 = () => {
+    const r2 = JSON.parse(JSON.stringify(ranges))
+    let foundSomeOverlappingChanges = true
+    const counter = {}
+    while (foundSomeOverlappingChanges) {
+        foundSomeOverlappingChanges = false
+        ranges = ranges.map(range => {
+            let [lower, upper] = range
+            if(counter[lower])delete counter[lower]
 
-const puzzle2 = () =>{
-    const myRanges = {}
+            r2.forEach(range2 => {
+                const [lower2, upper2] = range2
+                if(lower2 <= upper && lower <= upper2){
 
-    while (ranges.length >0){
-        let [lower, upper] = ranges.pop()
-        const filteredRanges = ranges.filter(range => {
-           return  !(range[0] >= upper || range[1] <= lower);
-        });
-        filteredRanges.forEach(range => {
-            const [l,u]= range;
-            if(l < lower)lower = l
-            if(u > upper)upper = u
-            const i = ranges.indexOf(range)
-            ranges.splice(i,1);
-
+                    if (lower2 < lower) {
+                        lower = lower2
+                        foundSomeOverlappingChanges = true
+                    }
+                    if (upper2 > upper) {
+                        upper = upper2
+                        foundSomeOverlappingChanges = true
+                    }
+                }
+            })
+            counter[lower] = upper;
+            return [lower, upper]
         })
-        // console.log('>>>>>',lower, upper, ranges);
-        // console.log('filteredRanges', filteredRanges);
-        myRanges[lower] = upper;
     }
     let sum = 0
-    // console.log(myRanges);
-    Object.entries(myRanges).forEach(([k,v])=> {
-        // console.log(Number(k),v,v-Number(k)+1);
-        sum += v-Number(k)+1
-    })
-    console.log(sum);
+    for (const key in counter) {
+        sum += counter[key] - key + 1;
+    }
+    console.log(sum)
 }
 
-puzzle2();
-
-// too high: 366825803001600
-// too low:  354446125840674
+puzzle2()
